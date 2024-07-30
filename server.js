9aa3a4f731084f593e4ca605c82d8d76d705eb22
ericdu6865 <zhengyuandu2005311@gmail.com>
2024-07-30 11:36:34 -0400

    updated

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", async (req, res)=>{
+    res.render("index.ejs");
+});
+
+app.listen(3000, () => {
+    console.log("Listening on Port 3000");
+});
+
